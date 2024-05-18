8e9114c7fb486f717d99f2a0e265a2827b5cd00b
Suvarna <suvarnapathepur@gmail.com>
2024-05-18 17:39:25 -0400

    added product controller and customer controller

---
+++ BeSpokedBikesAPI/Controllers/CustomersController.cs
@@ -0,0 +1,59 @@
+﻿using BeSpokedBikesAPI.Interface;
+using BeSpokedBikesAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeSpokedBikesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomersController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var customers = await _customerRepository.GetAll();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            var customer = await _customerRepository.GetById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(Customer customer)
+        {
+            await _customerRepository.Add(customer);
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            var existingCustomer = await _customerRepository.GetById(id);
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+            customer.Id = id;
+            await _customerRepository.Update(customer);
+            return NoContent();
+        }
+    }
+}
+
---
+++ BeSpokedBikesAPI/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using BeSpokedBikesAPI.Interface;
+using BeSpokedBikesAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeSpokedBikesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            Console.WriteLine(Url);
+            var products = await _productRepository.GetAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            await _productRepository.Add(product);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            var existingProduct = await _productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            product.Id = id;
+            await _productRepository.Update(product);
+            return NoContent();
+        }
+    }
+
+}
